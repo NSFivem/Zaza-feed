547cc4c376e7fb2dc32b10a98368ef51989f6a71
NS100 <86939732+NSFivem@users.noreply.github.com>
2021-12-22 19:59:06 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'NS100#0001 / Emerge scripts'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js',
+    '/html/img/*.webp',
+    '/html/img/*.png',
+}
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
